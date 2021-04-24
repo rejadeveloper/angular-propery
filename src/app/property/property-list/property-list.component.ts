2135d7166f70f433d24fc483f4e602c20832e3bb
rejabul <rejabul.ansary@indusnet.co.in>
2021-04-24 18:27:09 +0530

    add property form building

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from 'src/app/services/housing.service';
+import { IProperty } from '../IProperty.Interface';
 
 @Component({
   selector: 'app-property-list',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route:ActivatedRoute, private houseingService:HousingService) { }
+
+  SellRent=1;//buy default
+  properties:Array<IProperty>;
 
   ngOnInit(): void {
+    if(this.route.snapshot.url.toString()){
+      this.SellRent=2;//set rent when its value
+    }
+    this.houseingService.getAllProperties(this.SellRent).subscribe(
+      data=>{
+        this.properties=data;
+        console.log(data);
+
+      },
+      error=>{
+        console.log('error...')
+      }
+    );
   }
 
 }
